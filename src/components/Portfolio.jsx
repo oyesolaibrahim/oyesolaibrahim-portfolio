69dc6bb327ea148688dc9421626d373f55009ba6
Ibrahim <oyesolaibrahim14@gmail.com>
2024-03-20 15:48:56 +0100

    Change content in some done projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -200,24 +200,31 @@ return (
         </section>
 	</main>
     <div id="horizontal-line"></div> 
-     
-    <nav style={{backgroundColor: theme.navBackgroundColor }} className="nav2">
-        <div className="home-home column home-column" style={{backgroundColor: theme.navDivbackgroundColor }}>
-            <i className="fa fa-home fa-2x" aria-hidden="true"></i>
-            <Link to={"/"}>Home</Link>
-        </div>
-        <div className="home-about-me column home-column" style={{backgroundColor: theme.navDivbackgroundColor }}>
-            <i className="fa fa-address-book fa-2x" aria-hidden="true"></i>
-            <Link to={"/About"}>About Me</Link>
-        </div>
-        <div className="home-portfolio column home-column" style={{backgroundColor: theme.navDivbackgroundColor }}>
-            <i className="fa fa-briefcase fa-2x" aria-hidden="true"></i>
-            <Link to={"/Portfolio"}>Portfolio</Link>
-        </div>
-        <div className="home-contact column home-column" style={{backgroundColor: theme.navDivbackgroundColor }}>
-            <i className="fa fa-phone-square fa-2x" aria-hidden="true"></i>
-            <Link to={"/Contact"}>Contact</Link>
-        </div>
+    <nav style={{backgroundColor: theme.navBackgroundColor}} className="nav2">
+            <Link to={"/"} style={{color: "white"}} className="home-home">
+            <div className="column home-column" style={{backgroundColor: theme.navDivbackgroundColor}}>
+                <i className="fa fa-home fa-2x" aria-hidden="true"></i>
+                Home
+            </div>
+            </Link>
+            <Link to={"/About"} style={{color: "white"}} className="home-about-me">
+            <div className="column home-column" style={{backgroundColor: theme.navDivbackgroundColor}}>
+                <i className="fa fa-address-book fa-2x" aria-hidden="true"></i>
+                About Me
+            </div>
+            </Link>
+            <Link to={"/Portfolio"} style={{color: "white"}} className="home-portfolio">
+            <div className="column home-column" style={{backgroundColor: theme.navDivbackgroundColor}}>
+                <i className="fa fa-briefcase fa-2x" aria-hidden="true"></i>
+                Portfolio
+            </div>
+            </Link>
+             <Link to={"/Contact"} style={{color: "white"}} className="home-contact">
+             <div className="column home-column" style={{backgroundColor: theme.navDivbackgroundColor}}>
+                <i className="fa fa-phone-square fa-2x" aria-hidden="true"></i>
+                Contact
+            </div>
+            </Link>
      </nav> 
 </body>
 
